cf92ef5a3cc856c816ebc5f0a44e7bb94764c06c
Alfredo Morales Sánchez <bluesmoss@gmail.com>
2020-04-07 17:38:19 -0500

    feature(Chat): Add endpoints to list(get) and save (post) new chats

--- network/routes.js
+++ network/routes.js
@@ -1,7 +1,9 @@
 const user = require('../components/user/network');
+const chat = require('../components/chat/network');
 
 const routes = server => {
     server.use('/user', user);
+    server.use('/chat', chat);
 }
 
 module.exports = routes;

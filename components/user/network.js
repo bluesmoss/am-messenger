a4142129d6b62b18a90bb44245599e3395307c84
Alfredo Morales Sánchez <bluesmoss@gmail.com>
2020-04-07 16:02:19 -0500

    feature(User): Add endpoints to list(get) and save (post) new users

---
+++ components/user/network.js
@@ -0,0 +1,32 @@
+const express = require('express'); //Servidor
+const router = express.Router(); //Routes
+const response = require('../../network/response');
+const controller = require('./controller');
+
+
+router.get('/', function (req, res){
+    const filterUser = req.query.name || null;
+    controller.getUsers(filterUser)
+    .then( userList => {
+        response.success(req, res, userList, 200); 
+    })
+    .catch(e => {
+        response.error(req, res, 'Unexpected error', 500, e);  
+    });    
+});
+
+
+router.post('/', function (req, res){
+    controller.addUser(req.body.name, req.body.email)
+    .then( data => {
+        response.success(req, res, data, 201); 
+    })
+    .catch(e => {
+        response.error(req, res, 'Invalid format', 400, e);  
+    });
+
+});
+
+
+
+module.exports = router;
--- network/routes.js
+++ network/routes.js
@@ -1,5 +1,7 @@
+const user = require('../components/user/network');
+
 const routes = server => {
-   
+    server.use('/user', user);
 }
 
 module.exports = routes;
